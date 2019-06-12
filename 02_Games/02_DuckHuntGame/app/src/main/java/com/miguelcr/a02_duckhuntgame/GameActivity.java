dcf285e84c08fce114b85c24f495af26777d9a79
Miguel Campos <camposmiguel@gmail.com>
2019-06-12 13:12:44 +0200

    Final

--- 02_Games/02_DuckHuntGame/app/src/main/java/com/miguelcr/a02_duckhuntgame/GameActivity.java
+++ 02_Games/02_DuckHuntGame/app/src/main/java/com/miguelcr/a02_duckhuntgame/GameActivity.java
@@ -3,6 +3,7 @@ package com.miguelcr.a02_duckhuntgame;
 import android.graphics.Point;
 import android.os.CountDownTimer;
 import android.os.Handler;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
@@ -12,6 +13,11 @@ import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.FirebaseFirestore;
+
 import java.util.Random;
 
 public class GameActivity extends AppCompatActivity implements View.OnClickListener {
@@ -20,6 +26,9 @@ public class GameActivity extends AppCompatActivity implements View.OnClickListe
     int ducksHunted = 0;
     boolean gameOver = false;
     String nick;
+    FirebaseFirestore db;
+    User user;
+    String userId;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -46,6 +55,28 @@ public class GameActivity extends AppCompatActivity implements View.OnClickListe
         Bundle extras = getIntent().getExtras();
         nick = extras.getString("nickname");
         tvNick.setText(nick);
+
+        // Connection to the firestore database
+        db = FirebaseFirestore.getInstance();
+
+        // Create the user to save in the db
+        user = new User(nick, 0);
+
+        // Save the nick into the database
+        db.collection("users")
+                .add(user)
+                .addOnSuccessListener(new OnSuccessListener<DocumentReference>() {
+                    @Override
+                    public void onSuccess(DocumentReference documentReference) {
+                        userId = documentReference.getId();
+                    }
+                })
+                .addOnFailureListener(new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception e) {
+
+                    }
+                });
     }
 
     private void startCountDown() {
@@ -58,8 +89,29 @@ public class GameActivity extends AppCompatActivity implements View.OnClickListe
             public void onFinish() {
                 tvTimer.setText("0s");
                 gameOver = true;
+                gameOverData();
             }
         }.start();
+    }
+
+    private void gameOverData() {
+        user.setDucks(ducksHunted);
+
+        db.collection("users")
+                .document(userId)
+                .set(user)
+                .addOnSuccessListener(new OnSuccessListener<Void>() {
+                    @Override
+                    public void onSuccess(Void aVoid) {
+
+                    }
+                })
+                .addOnFailureListener(new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception e) {
+
+                    }
+                });
     }
 
     @Override
